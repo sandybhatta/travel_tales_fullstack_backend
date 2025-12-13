56e3da2e0dadd730f40dfd8de1f488e25f3f73f5
sandybhatta <sandipresponse256@gmail.com>
2025-12-13 12:14:34 +0530

    multer middleware changed to accept .jpg image also

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -6,7 +6,7 @@ export const upload = multer({
   storage,
   limits: { fileSize: 100 * 1024 * 1024 }, // max 100MB
   fileFilter: (req, file, cb) => {
-    const allowedTypes = ["image/jpeg", "image/png", "image/webp", "video/mp4"];
+    const allowedTypes = ["image/jpeg","image/jpg", "image/png", "image/webp", "video/mp4"];
     if (allowedTypes.includes(file.mimetype)) cb(null, true);
     else cb(new Error("Only images and MP4 videos allowed!"));
   }
