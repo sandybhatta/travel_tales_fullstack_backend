de035b0f5888f386c339564c65aa97f24c529b41
sandybhatta <sandipresponse256@gmail.com>
2025-12-17 04:14:05 +0530

    refactored adddPostToTrip to accept multiple and unique posts in a trip

--- Controllers/trip.controllers/addPostToTrip.js
+++ Controllers/trip.controllers/addPostToTrip.js
@@ -1,67 +1,75 @@
+
+import Post from "../../models/Post.js";
 import Trip from "../../models/Trip.js";
 
 const addPostToTrip = async (req, res) => {
+ 
+
   try {
     const { tripId } = req.params;
-    // , , dayNumber, isHighlighted
-
-    const { posts  } = req.body;
+    const { posts } = req.body;
     const userId = req.user._id;
 
+    if (!Array.isArray(posts) || posts.length === 0) {
+      return res.status(400).json({ message: "Posts array is required" });
+    }
 
-   
-
-    const trip = await Trip.findById(tripId);
+    const trip = await Trip.findById(tripId)
     if (!trip) {
       return res.status(404).json({ message: "Trip not found" });
     }
 
     if (!trip.canPost(req.user)) {
-      return res.status(403).json({ message: "Not allowed to add post to this trip" });
+      return res.status(403).json({ message: "Not allowed" });
     }
 
- 
-    
-    let tripPosts = []
+    const existingPostIds = new Set(
+      trip.posts.map(p => p.post.toString())
+    );
 
-    for( let post of posts ){
-      let alreadyExists = false;
+    const newTripPosts = [];
+    const postIdsToUpdate = [];
 
-      if (!post._id) {
-        return res.status(400).json({ error: "postId is required" });
+    for (const post of posts) {
+      if (!post._id) continue;
+
+      if (!existingPostIds.has(post._id.toString())) {
+        newTripPosts.push({
+          post: post._id,
+          dayNumber: post.dayNumber ?? null,
+          isHighlighted: !!post.isHighlighted,
+          highlightedBy: post.isHighlighted ? userId : null,
+        });
+
+        postIdsToUpdate.push(post._id);
       }
-      alreadyExists = trip.posts?.some(p => p.post.toString() === post._id.toString());
-      if(!alreadyExists){
+    }
+
+    if (postIdsToUpdate.length) {
+      await Post.updateMany(
+        { _id: { $in: postIdsToUpdate } },
+        { $set: { tripId } },
         
-        const postObj={
-          ...post,
-          highlightedBy:post.isHighlighted ? userId : null,
-        }
-        tripPosts.push(postObj)
-      }
+      );
     }
 
-
-     
-
-    
- 
-    trip.posts = [...trip.posts, ...tripPosts]
-
+    trip.posts.push(...newTripPosts);
     await trip.save();
 
+   
+
     return res.status(200).json({
-      message: "Post added to trip successfully",
+      message: "Posts added successfully",
       trip,
     });
 
   } catch (err) {
+    
     return res.status(500).json({
       message: "Internal Server Error",
       error: err.message,
     });
-  }
+  } 
 };
 
-
-export default addPostToTrip
+export default addPostToTrip;
